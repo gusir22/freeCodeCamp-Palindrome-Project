c897d8a7697468a08e211a9d2b5b705cc1bb45e7
Gustavo Rivero <gusir22@gmail.com>
2025-04-18 00:14:48 -0400

    Cleaned up JS file and added comments

--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,17 @@
 function validateInput(form){
-    let phrase = form['text-input'].value;
+    /* This function checks if the user has entered a value to #text-input
+    field before executing the checkForPalidrome() function.
+    If the user provides an empty value, the function alert the user to enter
+    a value to the #text-input field. */
 
+
+    let phrase = form['text-input'].value; // get text-input value
+
+    // check if phrase value is empty or not
     if (phrase == ""){
-        alert("Please input a value");
+        alert("Please input a value"); // alert user if empty
     } else {
-        checkForPalindrome(phrase);
+        checkForPalindrome(phrase); // redirect user to checkForPalindrome if not empty
     }
 
     
@@ -18,6 +25,8 @@ function reverseStr(str) {
 }
 
 function checkForPalindrome(phrase) {
+    /* Checks if a phrase is a palindrome and then outputs the result
+    to the #result element in index.html */
 
     // change phrase to lower case and replace all non-alphanumeric chars with nothing
     let originalPhrase = phrase; // store original phrase for result message output
@@ -26,6 +35,7 @@ function checkForPalindrome(phrase) {
 
     let resultMessage; // init empty result message
 
+    // check if phrase and mirror match
     if (phrase == mirror) {
         resultMessage = originalPhrase + " is a palindrome";
     } else {
@@ -33,8 +43,4 @@ function checkForPalindrome(phrase) {
     }
 
     document.getElementById('result').innerText = resultMessage; // assign results
-
-
-    console.log(`Phrase: ${phrase}`);
-    console.log(`Mirror: ${mirror}`);
 }
