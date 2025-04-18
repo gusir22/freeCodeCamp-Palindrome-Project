e57093791763dd708f182c6a58d9e1a537887ed0
Gustavo Rivero <gusir22@gmail.com>
2025-04-17 23:33:05 -0400

    Created validateInput() for empty inputs

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,15 @@
-console.log("Hello world!");
+function validateInput(form){
+    let phrase = form['text-input'].value;
+
+    if (phrase == ""){
+        alert("Please input a value");
+    } else {
+        checkForPalindrome(phrase);
+    }
+
+    
+}
+
+function checkForPalindrome(phrase) {
+    console.log(phrase);
+}
