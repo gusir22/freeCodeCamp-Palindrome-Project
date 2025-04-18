717775c5f284ace671479c102c17b8c239bae41c
Gustavo Rivero <gusir22@gmail.com>
2025-04-18 00:11:11 -0400

    Completed checkForPalindrome function to compare phrase to mirror and provide results to the user

--- static/js/script.js
+++ static/js/script.js
@@ -13,15 +13,26 @@ function validateInput(form){
 function reverseStr(str) {
     /* This function reverses strings by turning them into arrays, reversing them, and re-joining
     them into a string again before returning the end result */
-    
+
     return str.split('').reverse().join('');
 }
 
 function checkForPalindrome(phrase) {
 
     // change phrase to lower case and replace all non-alphanumeric chars with nothing
+    let originalPhrase = phrase; // store original phrase for result message output
     phrase = phrase.toLowerCase().replaceAll(/[^a-z0-9]/g,"");
-    mirror = reverseStr(phrase); // create mirror to compare phrase to
+    let mirror = reverseStr(phrase); // create mirror to compare phrase to
+
+    let resultMessage; // init empty result message
+
+    if (phrase == mirror) {
+        resultMessage = originalPhrase + " is a palindrome";
+    } else {
+        resultMessage = originalPhrase + " is not a palindrome";
+    }
+
+    document.getElementById('result').innerText = resultMessage; // assign results
 
 
     console.log(`Phrase: ${phrase}`);
